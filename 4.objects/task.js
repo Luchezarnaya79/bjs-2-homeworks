d44ab4528b58c389dbd597c1ffacb945e3b9431b
Luchezarnaya79 <luchezarnaya79olga79@gmail.com>
2023-10-05 21:15:15 +0300

    Четвертое задание выполнено полностью

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,34 @@
+"use strict"
+
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksAdd) {
+  if (!this.marks){
+        console.log('Студент был исключен');
+        return;
+  }
+    this.marks.push(...marksAdd);
 }
 
 Student.prototype.getAverage = function () {
-  
+  if (!this.marks || !this.marks.length){
+    return 0;
+  }
+  const sum = this.marks.reduce((acc, curr) => acc + curr);
+  return sum / this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  this.exclude = reason;
 }
